9d34e8978ba3f91f2dedc9ba92e03a05400ba35e
martyminion <martkimwaweru@gmail.com>
2020-03-31 12:19:11 +0300

    adds logic to highlight highest upvote to a quote

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,32 @@
-import { Directive } from '@angular/core';
-
+import { Directive, ElementRef, Input } from '@angular/core';
+import { QuoteEntry } from './quote-entry'
+//import { QuoteComponent } from './quote/quote.component'
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  @Input() quotes:QuoteEntry[];
+  
+  constructor(private elem:ElementRef) { }
 
+    check(){
+      let quoteUpvotes = []
+      for(let j = 0; j<this.quotes.length; j++)
+      {
+        quoteUpvotes.push(this.quotes[j].upvotes)
+      }
+  
+      quoteUpvotes.sort(function(a,b){return b-a})
+  
+      for(let j = 0; j<this.quotes.length; j++)
+      {
+        if (quoteUpvotes[0]=== this.quotes[j].upvotes){
+          this.elem.nativeElement.style.background ='yellow'
+        }
+      }
+    }
+   
+  
+ 
 }
